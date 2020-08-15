5ca72bb8d6aa09f73484154f304fc81856925f1e
chenkunsheng <chenkunsheng@sailvan.com>
2020-08-15 21:35:53 +0800

    #

--- public/media/share/simple-share.js
+++ public/media/share/simple-share.js
@@ -6,4 +6,77 @@
  * Licensed under the MIT license.
  */
 
-var SimpleShare=function(t){function e(t){return t=t.replace("{url}",n),t=t.replace("{title}",i),t=t.replace("{content}",o),t=t.replace("{pic}",c)}t=t||{};var n=t.url||window.location.href,i=t.title||document.title,o=t.content||"",c=t.pic||"";n=encodeURIComponent(n),i=encodeURIComponent(i),o=encodeURIComponent(o),c=encodeURIComponent(c);var r="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={url}&title={title}&pics={pic}&summary={content}",p="http://service.weibo.com/share/share.php?url={url}&title={title}&pic={pic}&searchPic=false",l="http://share.v.t.qq.com/index.php?c=share&a=index&url={url}&title={title}&appkey=801cf76d3cfc44ada52ec13114e84a96",u="http://widget.renren.com/dialog/share?resourceUrl={url}&srcUrl={url}&title={title}&description={content}",h="http://www.douban.com/share/service?href={url}&name={title}&text={content}&image={pic}",a="https://www.facebook.com/sharer/sharer.php?u={url}&t={title}&pic={pic}",s="https://twitter.com/intent/tweet?text={title}&url={url}",w="https://www.linkedin.com/shareArticle?title={title}&summary={content}&mini=true&url={url}&ro=true",d="http://qr.liantu.com/api.php?text={url}",m="http://connect.qq.com/widget/shareqq/index.html?url={url}&desc={title}&pics={pic}";this.qzone=function(){window.open(e(r))},this.weibo=function(){window.open(e(p))},this.tqq=function(){window.open(e(l))},this.renren=function(){window.open(e(u))},this.douban=function(){window.open(e(h))},this.facebook=function(){window.open(e(a))},this.twitter=function(){window.open(e(s))},this.linkedin=function(){window.open(e(w))},this.qq=function(){window.open(e(m))},this.weixin=function(t){t?t(e(d)):window.open(e(d))}};
+var SimpleShare = function (options) {
+
+	// get share content
+	options = options || {};
+	var url = options.url || window.location.href;
+	var title = options.title || document.title;
+	var content = options.content || '';
+	var pic = options.pic || '';
+
+	// fix content format
+	url = encodeURIComponent(url);
+	title = encodeURIComponent(title);
+	content = encodeURIComponent(content);
+	pic = encodeURIComponent(pic);
+
+	// share target url
+	var qzone = 'http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url={url}&title={title}&pics={pic}&summary={content}';
+	var weibo = 'http://service.weibo.com/share/share.php?url={url}&title={title}&pic={pic}&searchPic=false';
+	var tqq = 'http://share.v.t.qq.com/index.php?c=share&a=index&url={url}&title={title}&appkey=801cf76d3cfc44ada52ec13114e84a96';
+	var renren = 'http://widget.renren.com/dialog/share?resourceUrl={url}&srcUrl={url}&title={title}&description={content}';
+	var douban = 'http://www.douban.com/share/service?href={url}&name={title}&text={content}&image={pic}';
+	var facebook = 'https://www.facebook.com/sharer/sharer.php?u={url}&t={title}&pic={pic}';
+	var twitter = 'https://twitter.com/intent/tweet?text={title}&url={url}';
+	var linkedin = 'https://www.linkedin.com/shareArticle?title={title}&summary={content}&mini=true&url={url}&ro=true';
+	var weixin = 'http://qr.liantu.com/api.php?text={url}';
+	var qq = 'http://connect.qq.com/widget/shareqq/index.html?url={url}&desc={title}&pics={pic}';
+
+	// replace content functions
+	function replaceAPI (api) {
+		api = api.replace('{url}', url);
+		api = api.replace('{title}', title);
+		api = api.replace('{content}', content);
+		api = api.replace('{pic}', pic);
+
+		return api;
+	}
+
+	// share target
+	this.qzone = function() {
+		window.open(replaceAPI(qzone));
+	};
+	this.weibo = function() {
+		window.open(replaceAPI(weibo));
+	};
+	this.tqq = function() {
+		window.open(replaceAPI(tqq));
+	};
+	this.renren = function() {
+		window.open(replaceAPI(renren));
+	};
+	this.douban = function() {
+		window.open(replaceAPI(douban));
+	};
+	this.facebook = function() {
+		window.open(replaceAPI(facebook));
+	};
+	this.twitter = function() {
+		window.open(replaceAPI(twitter));
+	};
+	this.linkedin = function() {
+		window.open(replaceAPI(linkedin));
+	};
+	this.qq = function() {
+		window.open(replaceAPI(qq));
+	};
+	this.weixin = function(callback) {
+		if (!callback) {
+			window.open(replaceAPI(weixin));
+		}else{
+			callback(replaceAPI(weixin));
+		}
+	};
+
+};
